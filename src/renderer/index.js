197c89638e2c44b5bf0008129319465f532c5580
Orta Therox <orta.therox@gmail.com>
2018-07-26 12:41:27 -0400

    Initial commit

---
+++ src/renderer/index.js
@@ -0,0 +1,19 @@
+const webview = document.createElement("webview")
+webview.src = "https://cms.artsy.net";
+webview.allowpopups = true
+
+const style = webview.style
+style.position = "absolute"
+style.width = "100%"
+style.height = "100%"
+style.top = 0
+style.left = 0
+style.display = "inline-flex !important"
+
+const load = () => {
+  webview.removeEventListener('did-finish-load', load);
+
+};
+
+webview.addEventListener('did-finish-load', load)
+document.body.appendChild(webview)
